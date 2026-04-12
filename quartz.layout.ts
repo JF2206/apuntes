1bbe20c63f6b13de504e19416b220d4e507b58c8
JF2206 <87577785+JF2206@users.noreply.github.com>
2026-04-12 20:58:35 +0200

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -23,7 +23,10 @@ export const defaultContentPageLayout: PageLayout = {
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
-    Component.TableOfContents(),
+    Component.ClassName({
+      className: "toc-static",
+      component: Component.TableOfContents(),
+    }),
   ],
   left: [
     Component.PageTitle(),
